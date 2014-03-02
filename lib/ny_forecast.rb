5e42624c8393a7e2f7e474fceacd3a22a84350cb
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-03-01 21:54:46 -0500

    complete initial app, begin gem

---
+++ lib/ny_forecast.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'open-uri'
+
+class NYForecast
+  def initialize
+    @doc = Nokogiri::HTML(open("http://www.weather.com/weather/hourbyhour/graph/New+York+NY+USNY0996:1:US"))
+    scrape_forecast
+  end
+
+
+  def scrape_forecast
+    
+    
+    forecasts = @doc.css("div.wx-timepart").collect { |section| section.text.gsub(/\n/,'') }
+    forecasts.each_with_index do |forecast, i|
+      if i % 4 == 0
+        puts forecast.gsub("AM","AM\n").gsub("PM","PM\n").gsub("FEE", "\nFEE").gsub("°", "°\n").gsub("%", "%\n").gsub("Show 15 Minute Details","").gsub("mph","mph\n\n")
+      end
+    end
+
+    
+  end
+
+end
+
+NYForecast.new
---
+++ ny_forecast.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'ny_forecast'
+  s.version     = '0.0.0'
+  s.date        = '2014-03-01'
+  s.summary     = "NY weather forecast"
+  s.description = "Gathers next-6-hour weather forecast for NYC from weather.com"
+  s.authors     = ["Chris Kohlbrenner"]
+  s.email       = 'chris.kohlbrenner@gmail.com'
+  s.files       = ["lib/ny_forecast.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/ny_forecast'
+  s.license       = 'MIT'
+end
