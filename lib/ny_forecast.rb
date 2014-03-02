98da89169df89bfcc99745edf1f994d145f7f082
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-03-01 23:29:31 -0500

    remove day and date from returned forecast

--- lib/ny_forecast.rb
+++ lib/ny_forecast.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 class NYForecast
   def initialize
@@ -9,12 +10,17 @@ class NYForecast
 
 
   def scrape_forecast
-    
-    
+    to_delete = @doc.css("span.wx-date-label").collect {|x| x.text} + @doc.css("span.wx-day-label").collect {|x| x.text}
+
     forecasts = @doc.css("div.wx-timepart").collect { |section| section.text.gsub(/\n/,'') }
+    
     forecasts.each_with_index do |forecast, i|
+      to_delete.each do |word|
+        forecast.gsub!("#{word}", "")
+      end
+    
       if i % 4 == 0
-        puts forecast.gsub("AM","AM\n").gsub("PM","PM\n").gsub("FEE", "\nFEE").gsub("°", "°\n").gsub("%", "%\n").gsub("Show 15 Minute Details","").gsub("mph","mph\n\n")
+        puts forecast.gsub("°F", "°").gsub("AM","AM\n").gsub("PM","PM\n").gsub("FEE", "\nFEE").gsub("°", "°\n").gsub("%", "%\n").gsub("Show 15 Minute Details","").gsub("mph","mph\n\n")
       end
     end
 
@@ -22,3 +28,7 @@ class NYForecast
   end
 
 end
+
+NYForecast.new
+
+# .gsub(/"#{@doc.css("span.wx-date-label").text}"{3}/,"")
