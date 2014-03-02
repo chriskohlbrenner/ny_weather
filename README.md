7c3eab334c1cc1f9819df6e7fbb7e02401c11690
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-03-01 23:49:19 -0500

    add Readme

---
+++ README.md
@@ -0,0 +1,17 @@
+## Welcome to NY Forecast.
+
+NY Forecast is a simple gem for viewing 6-hour weather forecasts for New York City. It scrapes the forecast from Weather.com and displays in the command line.
+
+## How to Use
+
+1. Install NY Forecast at the command prompt:
+
+        gem install ny_forecast
+
+2. At the command prompt, view the forecast:
+
+        ny_forecast
+
+## License
+
+Ruby on Rails is released under the [MIT License](http://www.opensource.org/licenses/MIT).
--- lib/ny_forecast.rb
+++ lib/ny_forecast.rb
@@ -1,13 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 class NYForecast
   def initialize
     @doc = Nokogiri::HTML(open("http://www.weather.com/weather/hourbyhour/graph/New+York+NY+USNY0996:1:US"))
+    intro
     scrape_forecast
   end
 
+  def intro
+    introduction = "\nToday's date: #{Date.today}.\nThe weather for the next six hours is as follows:\n\n"
+    puts introduction
+  end
 
   def scrape_forecast
     to_delete = @doc.css("span.wx-date-label").collect {|x| x.text} + @doc.css("span.wx-day-label").collect {|x| x.text}
@@ -18,17 +22,8 @@ class NYForecast
       to_delete.each do |word|
         forecast.gsub!("#{word}", "")
       end
-    
-      if i % 4 == 0
-        puts forecast.gsub("°F", "°").gsub("AM","AM\n").gsub("PM","PM\n").gsub("FEE", "\nFEE").gsub("°", "°\n").gsub("%", "%\n").gsub("Show 15 Minute Details","").gsub("mph","mph\n\n")
-      end
+
+      puts forecast.gsub("°F", "°").gsub("AM","AM\n").gsub("PM","PM\n").gsub("FEE", "\nFEE").gsub("°", "°\n").gsub("%", "%\n").gsub("Show 15 Minute Details","").gsub("mph","mph\n\n")  if i % 4 == 0
     end
-
-    
   end
-
 end
-
-NYForecast.new
-
-# .gsub(/"#{@doc.css("span.wx-date-label").text}"{3}/,"")
